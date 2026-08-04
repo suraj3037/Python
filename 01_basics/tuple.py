680b2d6d083762ba09331374c08efec354b2c211
suraj3037 <surajpatil3037@gmail.com>
2026-08-04 14:59:07 +0530

    tuple operations

--- 01_basics/tuple.py
+++ 01_basics/tuple.py
@@ -3,3 +3,7 @@ print(colors)
 
 print(colors[0])  # Output: red
 print(colors[-1])  # Output: purple
+
+more_colors=('orange', 'pink')
+combined_colors=colors+more_colors
+print(combined_colors)  # Output: ('red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink')
