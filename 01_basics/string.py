6d074f65bb5f11b10d0379ad950eef8efeb33040
suraj3037 <surajpatil3037@gmail.com>
2026-08-04 11:37:28 +0530

    Slicing

--- 01_basics/string.py
+++ 01_basics/string.py
@@ -5,3 +5,15 @@ print(name)
 first_char=name[0]
 print("First character:", first_char)
 print("Last character:", name[-1])
+
+slice_name=name[0:4]
+print("Slice of name:", slice_name)
+
+slice_reverse=name[::-1]
+print("Reversed name:", slice_reverse)
+
+slice_middle=name[2:6]
+print("Middle slice of name:", slice_middle)
+
+slice_with_step=name[0:7:2]
+print("Slice with step:", slice_with_step)
