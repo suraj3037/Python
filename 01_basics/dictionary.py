f369983ed78baef5f5c288c6e29d8282a3922ee7
suraj3037 <surajpatil3037@gmail.com>
2026-08-04 14:30:41 +0530

    Item removing methods in dectionary

--- 01_basics/dictionary.py
+++ 01_basics/dictionary.py
@@ -15,3 +15,10 @@ print(indexed_colors)  # Output: {1: 'red', 2: 'green', 3: 'blue', 4: 'yellow'}
 
 indexed_colors.pop(4)  # Removes the key-value pair with key 4
 print(indexed_colors)  # Output: {1: 'red', 2: 'green', 3: 'blue'}
+
+indexed_colors.popitem()  # Removes the last inserted key-value pair
+print(indexed_colors)  # Output: {1: 'red', 2: 'green'}
+
+del indexed_colors[2]  # Deletes the key-value pair with key 2
+print(indexed_colors)  # Output: {1: 'red'}
+
