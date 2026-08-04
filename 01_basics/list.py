b60d5f735862318237d76a6f2be5644b50c07042
suraj3037 <surajpatil3037@gmail.com>
2026-08-04 12:03:41 +0530

    List Basic operations

--- 01_basics/list.py
+++ 01_basics/list.py
@@ -1,1 +1,12 @@
 colors=['red', 'green', 'blue', 'yellow', 'purple']
+
+print(colors[0])  # Output: red
+print(colors[-1])  # Output: purple
+
+print(colors[1:4])  # Output: ['green', 'blue', 'yellow']
+
+for color in colors:
+    print(color, end=" ")
+
+print("")
+
