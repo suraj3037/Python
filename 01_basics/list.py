4fdea6fdc1cf0ac5a4a3684b5a64ed9dce0ced22
suraj3037 <surajpatil3037@gmail.com>
2026-08-04 12:21:56 +0530

    list operations

--- 01_basics/list.py
+++ 01_basics/list.py
@@ -18,3 +18,19 @@ print(colors)  # Output: ['orange', 'pink', 'cyan', 'blue', 'yellow', 'purple']
 
 if 'blue' in colors:
     print("Blue is in the list")
+
+colors.append('black')
+print(colors) 
+
+print("The popped color is:", colors.pop())  # Removes and returns the last item
+
+colors.remove('pink')  # Removes the first occurrence of 'pink'
+print(colors)  # Output: ['orange', 'cyan', 'blue', 'yellow',
+
+colors.sort()  # Sorts the list in ascending order
+
+colors.reverse()  # Reverses the order of the list
+
+colors.clear()  # Removes all items from the list
+
+colors.insert(1, 'white')  # Inserts 'white' at index 1
