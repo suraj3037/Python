c7c8f42935051aaf57d5730cea385f6d855fbb86
suraj3037 <surajpatil3037@gmail.com>
2026-08-04 14:38:34 +0530

    Dictionary in Dictionary

--- 01_basics/dictionary.py
+++ 01_basics/dictionary.py
@@ -22,3 +22,12 @@ print(indexed_colors)  # Output: {1: 'red', 2: 'green'}
 del indexed_colors[2]  # Deletes the key-value pair with key 2
 print(indexed_colors)  # Output: {1: 'red'}
 
+
+Items={
+    "colors": {1: "red", 2: "green", 3: "blue"},
+    "fruits": {1: "apple", 2: "banana", 3: "cherry"}
+}
+
+print(Items)  # Output: {'colors': {1: 'red', 2: 'green', 3: 'blue'}, 'fruits': {1: 'apple', 2: 'banana', 3: 'cherry'}}
+
+print(Items["colors"][1])  # Output: red
