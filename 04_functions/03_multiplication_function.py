1faa74965b18fd1733e77441cb10be7d9e6a88a7
Suraj Patil <surajpatil3037@gmail.com>
2026-08-05 21:42:21 +0530

    multiplication program

---
+++ 04_functions/03_multiplication_function.py
@@ -0,0 +1,8 @@
+def multiply(a,b):
+    return a*b
+
+
+multiplication=multiply(2,"b")
+multiplication2=multiply(2,"b")
+print("The multiplication of two numbers is: ",multiplication)
+print("The multiplication of two numbers is: ",multiplication2)
